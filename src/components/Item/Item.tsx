f68e7f4baec94f20670ddf378a06cb869873b4c1
Jiwon <hajw.study@gmail.com>
2024-04-05 18:37:46 +0900

    item 컴포넌트 추가

---
+++ src/components/Item/Item.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import styled from "styled-components";
+
+type Props = {
+  id: string;
+  name: string;
+  address: string;
+  recruitDate: string;
+  appStartDate: string;
+  appEndDate: string;
+};
+const Item = ({
+  id,
+  name,
+  address,
+  recruitDate,
+  appStartDate,
+  appEndDate,
+}: Props) => {
+  const route = useRouter();
+
+  return (
+    <Container>
+      <Image src='/images/sample.jpg' width={595} height={304} alt='image' />
+      <Content>
+        <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
+          <Link href={`/detail/${id}`}>
+            <div className='house-name'>{name}</div>
+          </Link>
+          <Image
+            src='/images/icons/arrow-right.svg'
+            width={20}
+            height={20}
+            alt='arrow'
+          />
+        </div>
+
+        <div style={{ fontWeight: "bold", marginTop: "30px" }}>주소</div>
+        <div className='address'>{address}</div>
+
+        <DateWrap>
+          <div className='title'>모집 공고일</div>
+          <div className='date'>{recruitDate}</div>
+        </DateWrap>
+        <DateWrap>
+          <div className='title'>청약 접수 기간</div>
+          <div className='date'>
+            {appStartDate} ~ {appEndDate}
+          </div>
+        </DateWrap>
+      </Content>
+    </Container>
+  );
+};
+
+export default Item;
+
+const Container = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+`;
+
+const Content = styled.div`
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  .house-name {
+    font-size: 18px;
+    font-weight: bold;
+  }
+  .address {
+    font-size: 14px;
+  }
+`;
+
+const DateWrap = styled.div`
+  margin-top: 20px;
+
+  .title {
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  .date {
+    font-size: 14px;
+  }
+`;
