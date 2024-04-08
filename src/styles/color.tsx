0f466328abe8a345928dfc22da1f0ed3dfe7f6ce
Jiwon <hajw.study@gmail.com>
2024-04-08 17:47:01 +0900

    color 컴포넌트 수정

--- src/styles/color.tsx
+++ src/styles/color.tsx
@@ -1,9 +1,17 @@
 export const color = {
-  1: "#EDEDE9",
-  2: "#D6CCC2",
-  3: "#F5EBE0",
-  4: "#E3D5CA",
-  5: "#D5BDAF",
-  black: "#02060D",
-  white: "#F5F6F7",
+  blue: {
+    navy: "#234E70",
+    blue: "#00539C",
+    skyBlue: "#8AAAE5",
+  },
+  yellow: {
+    yellow: "#FFD662",
+    lemonYellow: "#FBF8BE",
+  },
+  secondary: {
+    red: "#EC4D36",
+    green: "#295F2E",
+    black: "#02060D",
+    white: "#F5F6F7",
+  },
 };
