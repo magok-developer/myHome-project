6afb42e4a20db634b8bd4596b68f0eb81512c104
Jiwon <hajw.study@gmail.com>
2024-04-08 17:35:41 +0900

    지역 options 코드 추가

---
+++ public/static/static.ts
@@ -0,0 +1,72 @@
+import { AREA_CODE } from "../lib/enum";
+
+export const AREA_CODE_OPTIONS = [
+  {
+    value: AREA_CODE.SEOUL,
+    label: "서울",
+  },
+  {
+    value: AREA_CODE.GANGWON,
+    label: "강원",
+  },
+  {
+    value: AREA_CODE.DAEJEON,
+    label: "대전",
+  },
+  {
+    value: AREA_CODE.CHUNGNAM,
+    label: "충남",
+  },
+  {
+    value: AREA_CODE.SEJONG,
+    label: "세종",
+  },
+  {
+    value: AREA_CODE.CHUNGBUK,
+    label: "충북",
+  },
+  {
+    value: AREA_CODE.INCHEON,
+    label: "인천",
+  },
+  {
+    value: AREA_CODE.GYEONGGI,
+    label: "경기",
+  },
+  {
+    value: AREA_CODE.GWANGJU,
+    label: "광주",
+  },
+  {
+    value: AREA_CODE.JEONNAM,
+    label: "전남",
+  },
+  {
+    value: AREA_CODE.JEONBUK,
+    label: "전북",
+  },
+  {
+    value: AREA_CODE.BUSAN,
+    label: "부산",
+  },
+  {
+    value: AREA_CODE.GYEONGNAM,
+    label: "경남",
+  },
+  {
+    value: AREA_CODE.ULSAN,
+    label: "울산",
+  },
+  {
+    value: AREA_CODE.JEJU,
+    label: "제주",
+  },
+  {
+    value: AREA_CODE.DEAGU,
+    label: "대구",
+  },
+  {
+    value: AREA_CODE.GYEONGBUK,
+    label: "경북",
+  },
+];
