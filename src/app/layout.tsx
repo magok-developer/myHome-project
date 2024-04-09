93895739d0248343bc5763c6ca45759a3e92b2f9
Jiwon <hajw.study@gmail.com>
2024-04-09 14:55:45 +0900

    google 폰트 사용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
+import { Noto_Sans_KR } from "next/font/google";
 import Header from "@/components/Header/Header";
 import RootContainer from "@/components/RootContainer";
 import type { Metadata } from "next";
 import "../../public/reset.css";
 import { color } from "@/styles/color";
-import "@/styles/global.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "My Home",
   description: "My Home",
 };
+
+const noto = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function RootLayout({
   children,
@@ -17,7 +23,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body style={{ background: color.secondary.white, position: "relative" }}>
+      <body
+        style={{
+          background: color.secondary.white,
+          position: "relative",
+        }}
+        className={noto.className}
+      >
+        <script
+          type='text/javascript'
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_MAP_KEY}&libraries=services`}
+        ></script>
         <RootContainer>
           <Header />
           <div style={{ paddingTop: "60px" }}>{children}</div>
