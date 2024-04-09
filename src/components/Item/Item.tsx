24cb18b14eda688b17207968865f06d5201029d4
Jiwon <hajw.study@gmail.com>
2024-04-09 14:56:42 +0900

    useInfiniteQuery 이용해서 무한 스크롤 구현

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -2,36 +2,39 @@
 
 import { color } from "@/styles/color";
 import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import styled from "styled-components";
+import { useState } from "react";
+import styled from "@emotion/styled";
+import Modal from "../Modal/Modal";
+import { APT_DETAIL_RESPONSE } from "@/api/model";
+import MapComponent from "../Map/Map";
 
 type Props = {
   id: string;
-  name: string;
-  address: string;
-  recruitDate: string;
-  appStartDate: string;
-  appEndDate: string;
+  data: APT_DETAIL_RESPONSE;
 };
 const Item = ({
   id,
-  name,
-  address,
-  recruitDate,
-  appStartDate,
-  appEndDate,
-}: Props) => {
-  const route = useRouter();
 
+  data,
+}: Props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <Container>
       <Image src='/images/sample.jpg' width={550} height={304} alt='image' />
       <Content>
         <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
-          <Link href={`/detail/${id}`}>
-            <div className='house-name'>{name}</div>
-          </Link>
+          <div className='house-name' onClick={openModal}>
+            {data.HOUSE_NM}
+          </div>
+
           <Image
             src='/images/icons/arrow-right.svg'
             width={20}
@@ -41,19 +44,96 @@ const Item = ({
         </div>
 
         <div style={{ fontWeight: "bold", marginTop: "30px" }}>주소</div>
-        <div className='address'>{address}</div>
+        <div className='address'>
+          ({data.HSSPLY_ZIP}) {data.HSSPLY_ADRES}
+        </div>
 
         <DateWrap>
           <div className='title'>모집 공고일</div>
-          <div className='date'>{recruitDate}</div>
+          <div className='date'>{data.RCRIT_PBLANC_DE}</div>
         </DateWrap>
         <DateWrap>
           <div className='title'>청약 접수 기간</div>
           <div className='date'>
-            {appStartDate} ~ {appEndDate}
+            {data.RCEPT_BGNDE} ~ {data.RCEPT_ENDDE}
           </div>
         </DateWrap>
       </Content>
+      <Modal visible={isModalOpen} onClose={closeModal}>
+        <ModalContainer>
+          <LeftSection>
+            <h3>{data.HOUSE_NM}</h3>
+            <div style={{ fontSize: "14px" }}>
+              ({data.HSSPLY_ZIP}) {data.HSSPLY_ADRES}
+            </div>
+
+            <MapComponent address={data.HSSPLY_ADRES} />
+          </LeftSection>
+          <div className='line' />
+          <RightSection>
+            <div className='wrap'>
+              <h5>모집 공고일</h5>
+              <div className='period'>{data.RCRIT_PBLANC_DE}</div>
+            </div>
+            <div className='wrap'>
+              <h5>청약 접수 기간</h5>
+              <div className='period'>
+                {data.SPSPLY_RCEPT_BGNDE} ~ {data.RCEPT_ENDDE}
+              </div>
+            </div>
+            <div className='wrap'>
+              <h5>특별 공급 접수 기간</h5>
+              <div className='period'>
+                {data.SPSPLY_RCEPT_BGNDE} ~ {data.SPSPLY_RCEPT_ENDDE}
+              </div>
+            </div>
+            <div className='wrap'>
+              <h5>1순위 해당지역 접수 기간</h5>
+              <div className='period'>
+                {data.GNRL_RNK1_CRSPAREA_RCPTDE} ~{" "}
+                {data.GNRL_RNK1_CRSPAREA_ENDDE}
+              </div>
+            </div>
+            <div className='wrap'>
+              <h5>1순위 기타지역 접수 기간</h5>
+              <div className='period'>
+                {data.GNRL_RNK1_ETC_AREA_RCPTDE} ~{" "}
+                {data.GNRL_RNK1_ETC_AREA_ENDDE}
+              </div>
+            </div>
+            <div className='wrap'>
+              <h5>2순위 해당지역 접수 기간</h5>
+              <div className='period'>
+                {data.GNRL_RNK2_CRSPAREA_RCPTDE} ~{" "}
+                {data.GNRL_RNK2_CRSPAREA_ENDDE}
+              </div>
+            </div>
+            <div className='wrap'>
+              <h5>2순위 기타지역 접수 기간</h5>
+              <div className='period'>
+                {data.GNRL_RNK2_ETC_AREA_RCPTDE} ~{" "}
+                {data.GNRL_RNK2_ETC_AREA_ENDDE}
+              </div>
+            </div>
+          </RightSection>
+          <RightSection>
+            <div className='wrap'>
+              <h5>당첨자 발표일</h5>
+              <div className='period'>{data.PRZWNER_PRESNATN_DE}</div>
+            </div>
+            <div className='wrap'>
+              <h5>계약 시작일</h5>
+              <div className='period'>
+                {data.CNTRCT_CNCLS_BGNDE} ~ {data.CNTRCT_CNCLS_ENDDE}
+              </div>
+            </div>
+            <div className='wrap'>
+              <h5>입주 예정 월</h5>
+              <div className='period'>{data.MVN_PREARNGE_YM}</div>
+            </div>
+          </RightSection>
+        </ModalContainer>
+      </Modal>
     </Container>
   );
 };
@@ -74,6 +154,7 @@ const Content = styled.div`
   .house-name {
     font-size: 16px;
     font-weight: bold;
+    cursor: pointer;
     &:hover {
       color: ${color.blue.navy};
     }
@@ -92,5 +173,43 @@ const DateWrap = styled.div`
   }
   .date {
     font-size: 14px;
+  }
+`;
+
+const ModalContainer = styled.div`
+  display: flex;
+  padding: 50px;
+
+  .line {
+    width: 1px;
+    height: 600px;
+    background: #bebebe;
+    margin-left: 50px;
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const LeftSection = styled.div`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+const RightSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+
+  margin: 50px 50px 0 50px;
+
+  .wrap {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+  .period {
+    font-size: 12px;
   }
 `;
