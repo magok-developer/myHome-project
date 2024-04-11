e1d7b62ec41d2c93f63fea893422cfa653780263
Jiwon <hajw.study@gmail.com>
2024-04-11 18:13:18 +0900

    아파트 페이지 수정

--- src/app/apt/page.tsx
+++ src/app/apt/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { rest } from "@/api/rest";
 import { getAptSalesInfoDetail } from "@/api/api";
 import { APT_DETAIL_REQUEST } from "@/api/model";
@@ -11,61 +11,37 @@ import styled from "@emotion/styled";
 import SelectBox from "@/components/SelectBox/SelectBox";
 import { AREA_CODE_OPTIONS } from "../../../public/static/static";
 import useChangeSelect from "@/components/hook/useChangeSelect";
-import { color } from "@/styles/color";
 import Image from "next/image";
+import Input from "@/components/Input/Input";
+import { formatForAreaCode } from "../../../public/lib/formatForEnum";
 
 dotenv.config();
 
 const initialParams = {
   page: 1,
   perPage: 10,
+  cond: { "SUBSCRPT_AREA_CODE_NM::EQ": null },
+  serviceKey: decodeURIComponent(process.env.NEXT_PUBLIC_API_KEY ?? ""),
 };
 
 const Page = () => {
   const [params, setParams] = useState(initialParams);
   const { select, onChange: onChangeSelect, setSelect } = useChangeSelect(null);
-  const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryKey: [rest.get.aptSalesInfoDetail, params],
-      queryFn: ({ pageParam = initialParams.page }) =>
-        getAptSalesInfoDetail({
-          ...params,
-          page: pageParam,
-        } as APT_DETAIL_REQUEST),
-      getNextPageParam: (lastPage, allPages) => {
-        // 마지막 페이지가 모든 페이지 중 마지막 페이지인지 확인
-        if (lastPage.length < initialParams.perPage) {
-          return null; // 더 이상 데이터가 없음
-        }
-        return allPages.length + 1; // 다음 페이지 번호 반환
+
+  const { data } = useQuery({
+    queryKey: [rest.get.aptSalesInfoDetail, params],
+    queryFn: ({ queryKey }) =>
+      getAptSalesInfoDetail(queryKey[1] as APT_DETAIL_REQUEST),
+  });
+
+  useEffect(() => {
+    setParams({
+      ...params,
+      cond: {
+        "SUBSCRPT_AREA_CODE_NM::EQ": select,
       },
-      initialPageParam: initialParams.page, // 초기 페이지 매개변수 설정
     });
-
-  useEffect(() => {
-    // 새로운 지역 코드가 선택되면 params 초기화
-    setParams(initialParams);
   }, [select]);
-
-  const handleScroll = () => {
-    // 스크롤 이벤트 핸들러
-    if (
-      window.innerHeight + document.documentElement.scrollTop !==
-      document.documentElement.offsetHeight
-    ) {
-      return;
-    }
-    if (isLoading || isFetchingNextPage) {
-      return;
-    }
-    fetchNextPage(); // 다음 페이지 데이터 가져오기
-  };
-
-  useEffect(() => {
-    // 스크롤 이벤트 추가
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [isLoading, isFetchingNextPage]);
 
   return (
     <Container>
@@ -79,7 +55,7 @@ const Page = () => {
         아파트 분양 정보
       </Title>
       <FilterWrap>
-        <div>인풋</div>
+        <Input placeholder='아파트 검색' />
         <div style={{ display: "flex", gap: "20px" }}>
           <SelectBox
             options={AREA_CODE_OPTIONS}
@@ -88,19 +64,10 @@ const Page = () => {
           />
         </div>
       </FilterWrap>
-      {data?.pages.map((pageData, index) => (
-        <React.Fragment key={index}>
-          {pageData.map((item, index) => (
-            <AptItem
-              key={`${item.HOUSE_MANAGE_NO}_${index}`}
-              id={item.HOUSE_MANAGE_NO}
-              data={item}
-            />
-          ))}
-        </React.Fragment>
+
+      {data?.map((item, index) => (
+        <AptItem id={item.HOUSE_MANAGE_NO} data={item} />
       ))}
-      {isLoading && <div>로딩중</div>}
-      {!hasNextPage && !isLoading && <div>더이상 데이터가 없습니다.</div>}
     </Container>
   );
 };
@@ -124,6 +91,8 @@ const Title = styled.div`
 
 const FilterWrap = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: end;
   margin: 40px 0;
+
+  gap: 8px;
 `;
