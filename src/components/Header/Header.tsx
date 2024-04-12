9f73ff8499c39cdeba4179a701828c7de53c40b2
Jiwon <hajw.study@gmail.com>
2024-04-12 15:28:41 +0900

    header 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,27 +7,41 @@ import { useState } from "react";
 import styled from "@emotion/styled";
 import { usePathname } from "next/navigation";
 
-const router = [
+const menuData = [
   {
+    label: "분양 정보",
     path: "/apt",
-    label: "아파트",
-  },
-  {
-    path: "/etc",
-    label: "아파트 외",
+    subMenu: [
+      {
+        label: "아파트",
+        path: "/apt",
+      },
+      {
+        label: "무순위/잔여세대",
+        path: "/leftover",
+      },
+      {
+        label: "아파트 외",
+        path: "/etc",
+      },
+      {
+        label: "공공지원 민간 임대",
+        path: "/public",
+      },
+    ],
   },
 ];
 
 const Header = () => {
   const path = usePathname();
-  const [visible, setVisible] = useState(false);
+  const [openMenuIndex, setOpenMenuIndex] = useState(null);
 
-  const handleClickMenu = () => {
-    setVisible(!visible);
+  const handleMenuClick = (index: any) => {
+    setOpenMenuIndex(openMenuIndex === index ? null : index);
   };
 
-  const handleClickOther = () => {
-    setVisible(false);
+  const handleSubMenuClick = () => {
+    setOpenMenuIndex(null);
   };
 
   return (
@@ -45,23 +59,30 @@ const Header = () => {
         />
       </Link>
 
-      <Wrap>
-        <div onClick={handleClickMenu} style={{ cursor: "pointer" }}>
-          분양 정보
-        </div>
-        <Menu visible={visible}>
-          {router.map((item, index) => (
-            <Link href={item.path} key={`${item.path}_${index}`}>
-              <div onClick={handleClickOther} className='label'>
-                {item.label}
-              </div>
-            </Link>
-          ))}
-        </Menu>
-        <div>경쟁률 조회</div>
-        <div>메뉴</div>
-        <div>메뉴</div>
-      </Wrap>
+      <MenuList>
+        {menuData.map((menu, index) => (
+          <MenuItem key={index}>
+            <MenuLabel
+              onClick={() => handleMenuClick(index)}
+              className={openMenuIndex === index ? "active" : ""}
+            >
+              {menu.label}
+            </MenuLabel>
+            {openMenuIndex === index && (
+              <SubMenu>
+                {menu.subMenu.map((subMenu, subIndex) => (
+                  <Link href={subMenu.path} key={subIndex}>
+                    <div onClick={handleSubMenuClick} className='subMenuItem'>
+                      {subMenu.label}
+                    </div>
+                  </Link>
+                ))}
+              </SubMenu>
+            )}
+          </MenuItem>
+        ))}
+      </MenuList>
+
       <div></div>
     </Container>
   );
@@ -86,34 +107,52 @@ const Container = styled.div<{ isHomePage: boolean }>`
     isHomePage ? "#526D80" : color.secondary.white};
   color: ${({ isHomePage }) =>
     isHomePage ? color.secondary.white : color.secondary.black};
+
+  /* 추가된 부분 */
+  position: relative;
 `;
 
-const Wrap = styled.div`
+const MenuList = styled.div`
   display: flex;
   gap: 30px;
-  position: relative;
-  font-weight: bold;
 `;
-const Menu = styled.div<{ visible: boolean }>`
-  display: ${({ visible }) => (visible ? "flex" : "none")};
+
+const MenuItem = styled.div`
+  position: relative;
+`;
+
+const MenuLabel = styled.div`
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: bold;
+  &.active {
+    // 활성화된 메뉴에 대한 스타일링
+
+    color: ${color.blue.blue};
+  }
+`;
+
+const SubMenu = styled.div`
+  width: 120px;
+  text-align: center;
+  font-size: 14px;
+  display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
   gap: 20px;
   position: absolute;
   top: 30px;
-  left: -10px;
-  padding: 16px;
-
-  box-sizing: border-box;
-  color: ${color.secondary.black};
-
-  font-size: 14px;
-
-  border: 1px solid ${color.blue.blue};
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 10px;
   background-color: ${color.secondary.white};
+  border: 1px solid ${color.blue.blue};
   border-radius: 4px;
-  .label:hover {
-    color: ${color.blue.blue};
+
+  .subMenuItem {
+    cursor: pointer;
+    color: ${color.secondary.black};
+    &:hover {
+      color: ${color.blue.blue};
+    }
   }
 `;
